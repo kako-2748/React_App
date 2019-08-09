5926ba517db3c1a054a7e8ebadc20ed4a1ca5261
kakojunichi <2748661@gmail.com>
2019-08-09 12:25:47 +0900

    #4

--- step2/index.tsx
+++ step2/index.tsx
@@ -2,16 +2,19 @@ import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {useState}  from 'react'
 
-
-
+const AppChild =() => {
+  return (
+    <div>
+      <p>
+        <App />
+      </p>
+    </div>
+  )
+}
 const App =() => {
   const [data, setData] = useState('HELLO WORLD')
   return (
-    <div>
-      <p>{data}</p>
-    </div>
+    <App />
   )
-
 }
-  
-ReactDOM.render(<App />, document.getElementById('container'))
+ReactDOM.render(<AppChild />,document.getElementById('container'))
