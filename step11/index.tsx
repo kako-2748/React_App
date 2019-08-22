bedbfeb443f86ffb855da4a9b4e55802f155945e
kakojunichi <2748661@gmail.com>
2019-08-22 17:38:15 +0900

    #11

--- step11/index.tsx
+++ step11/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import {useState, useReducer}  from 'react'
+import {useState, useReducer ,useContext, createContext}  from 'react'
 
 /*index.tsxにインジケーター本体を設置する。
 インジケーターは変数「show」がtrueの時のみ表示させること
@@ -9,19 +9,24 @@ import {useState, useReducer}  from 'react'
 登録ボタン押下時にuseReducerでインジケーターの変数「show」をtrueにする。
 dispatchを使用して変数を変更すること
 登録ボタン押下後「5秒後」にインジケーターを非表示にする。*/
-const FormComponent = (_props:any) => {
+
+const FormComponent = () => {
+  const [name, age,gender] = useContext(FormContext)
+  const [changeName, changeAge, checkedMen, checkedWomen, showIndicator] = useContext(ChangeFormContext)
+  
   return (
     <div>
-    <input type="text" value={_props.name} placeholder="氏名" onChange={_props.changeName}/>
-    <input type="radio" name={_props.gender} value="男" onChange={_props.checkedMen} /><label htmlFor="gender">男</label>
-    <input type="radio" name={_props.gender} value="女" onChange={_props.checkedWomen} /><label htmlFor="gender">女</label>
-    <input type="text" value={_props.age} placeholder="年齢" onChange={_props.changeAge}/>
-    <button onClick={_props.clickButton}>登録</button>
+    <input type="text" value={name} placeholder="氏名" onChange={changeName} />
+    <input type="radio" name={gender} value="男" onChange={checkedMen} /><label htmlFor="gender">男</label>
+    <input type="radio" name={gender} value="女" onChange={checkedWomen} /><label htmlFor="gender">女</label>
+    <input type="text" value={age} placeholder="年齢" onChange={changeAge} />
+    <button onClick={showIndicator}>登録</button>
     </div>
   )
 }
+
 const initState = {show: false}
-const reducer = (state:any,action:any) => {
+const Reducer = (state:any,action:any) => {
   switch(action.type) {
     case 'showImg':
     return {show: true}
@@ -32,25 +37,13 @@ const reducer = (state:any,action:any) => {
   }
 }
 
+const FormContext = createContext([])
+const ChangeFormContext = createContext([() => {}])
 const App = () => {
   const [name, setName] = useState('')
-  const [gender, setGender] = useState('')
   const [age, setAge] = useState('')
-  
-  const changeName = (e:any) => {
-    setName(e.value)
-  }
-  const changeAge = (e:any) => {
-    setAge(e.target.value)
-  }
-  const checkedMen = () => {
-    setGender('男')
-  }
-  const checkedWomen = () => {
-    setGender('女')
-  }
-
-  const [state, dispatch] = useReducer(reducer,
+  const [gender, setGender] = useState('')
+  const [state, dispatch] = useReducer(Reducer,
     initState)
     let img:any = document.getElementById('image')
 
@@ -69,19 +62,31 @@ const App = () => {
       console.log('none')
     }
   }
+  const changeName = (e: any) => {
+    setName(e.target.value)
+  }
+  const changeAge = (e:any) => {
+    setAge(e.target.value)
+  }
+  const checkedMen = () => {
+    setGender('男')
+  }
+  const checkedWomen = () => {
+    setGender('女')
+  }
+
 
   return (
-    <div>
-      <FormComponent 
-      valueName={name} changeName={changeName}
-      valueAge={age} changeAge={changeAge} 
-      valueGender={gender} checkedMen={checkedMen}
-      checkedWomen={checkedWomen} clickButton={showIndicator}/>
-      <div　id="image" style={{display:'none'}}>
-      <img src="../img/ajax-loader.gif" />
-      </div>
-    </div>
+    <FormContext.Provider value={[name, age, gender]}>
+      <ChangeFormContext.Provider value={[changeName, changeAge, checkedMen, checkedWomen, showIndicator]}>
+        <FormComponent />
+        <div　id="image" style={{display:'none'}}>
+         <img src="../img/ajax-loader.gif" />
+        </div>
+       </ChangeFormContext.Provider>
+     </FormContext.Provider>
   )
 }
 ReactDOM.render(<App />,document.getElementById('container'))
+
 
